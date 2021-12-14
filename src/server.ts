a9b9eae28dcdd1bee78019cdeb30a7b854e76365
João Vitor da Silva <silva.joao1@outlook.com.br>
2021-12-13 22:30:18 -0300

    feat: create specification

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,13 @@
 import 'dotenv/config';
 import express from 'express';
 
-import { categoriesRoutes } from './routes/categories.routes';
-import { specificationsRoutes } from './routes/specifications.routes';
+import { router } from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/categories', categoriesRoutes);
-app.use('/specifications', specificationsRoutes);
+app.use(router);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server listen on ${process.env.PORT}`);
